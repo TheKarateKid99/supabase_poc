e417b640088520e985b868f4e44949ae49540a5a
Stefan <stefan@haefelesoftware.com>
2024-12-04 21:10:36 +0200

    Trying original file

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,54 +1,64 @@
-import { createClient } from '@supabase/supabase-js';
-import { NextResponse, type NextRequest } from 'next/server';
+import { createServerClient } from '@supabase/ssr'
+import { NextResponse, type NextRequest } from 'next/server'
 
 export async function updateSession(request: NextRequest) {
-    const { pathname } = request.nextUrl;
+    let supabaseResponse = NextResponse.next({
+        request,
+    })
 
-    // Exclude specific routes from authentication checks
+    const supabase = createServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {
+            cookies: {
+                getAll() {
+                    return request.cookies.getAll()
+                },
+                setAll(cookiesToSet) {
+                    cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
+                    supabaseResponse = NextResponse.next({
+                        request,
+                    })
+                    cookiesToSet.forEach(({ name, value, options }) =>
+                        supabaseResponse.cookies.set(name, value, options)
+                    )
+                },
+            },
+        }
+    )
+
+    // IMPORTANT: Avoid writing any logic between createServerClient and
+    // supabase.auth.getUser(). A simple mistake could make it very hard to debug
+    // issues with users being randomly logged out.
+
+    const {
+        data: { user },
+    } = await supabase.auth.getUser()
+
     if (
-        pathname.startsWith('/login') ||
-        pathname.startsWith('/auth') ||
-        pathname.startsWith('/webhook')
+        !user &&
+        !request.nextUrl.pathname.startsWith('/login') &&
+        !request.nextUrl.pathname.startsWith('/auth') &&
+        !request.nextUrl.pathname.startsWith('/webhook')
     ) {
-        return NextResponse.next();
+        // no user, potentially respond by redirecting the user to the login page
+        const url = request.nextUrl.clone()
+        url.pathname = '/login'
+        return NextResponse.redirect(url)
     }
 
-    const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-    const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-    const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-    // Retrieve the user's session token from cookies
-    const accessToken = request.cookies.get('sb-fmjugedxgtsftjznpxau-auth-token')?.value;
-    //console.log(accessToken);
-    if (!accessToken) {
-        console.log("Empty Access Token");
-        const url = request.nextUrl.clone();
-        url.pathname = '/login';
-        return NextResponse.redirect(url);
-    }
-    //const decodedToken = decodeJWT(accessToken);
-    const updatedToken = accessToken.replace(/^base64-/, '');
-    const decodedToken = decodeBase64(updatedToken);
-    // Verify the user session with the access token
-    const { data: { user }, error } = await supabase.auth.getUser(decodedToken.access_token);
-   
-    if (!user) {
-        console.log("Invalid User");
-        const url = request.nextUrl.clone();
-        url.pathname = '/login';
-        return NextResponse.redirect(url);
-    }
+    // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
+    // creating a new response object with NextResponse.next() make sure to:
+    // 1. Pass the request in it, like so:
+    //    const myNewResponse = NextResponse.next({ request })
+    // 2. Copy over the cookies, like so:
+    //    myNewResponse.cookies.setAll(supabaseResponse.cookies.getAll())
+    // 3. Change the myNewResponse object to fit your needs, but avoid changing
+    //    the cookies!
+    // 4. Finally:
+    //    return myNewResponse
+    // If this is not done, you may be causing the browser and server to go out
+    // of sync and terminate the user's session prematurely!
 
-    // Proceed to the next middleware or route
-    return NextResponse.next();
+    return supabaseResponse
 }
-
-function decodeBase64(encoded: string) {
-    try {
-        const decoded = atob(encoded);
-        return JSON.parse(decoded);
-    } catch (error) {
-        console.error('Error decoding Base64:', error);
-        return null;
-    }
-}
-
